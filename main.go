c7531db15ce6a29398974feec0e57ea55878336d
Swaymaw <swaymaw@gmail.com>
2026-06-09 08:51:17 +0530

    fix: added cors wrapper on mux to avoid cross origin issues for local deployment

--- main.go
+++ main.go
@@ -10,6 +10,22 @@ import (
 	"github.com/modelcontextprotocol/go-sdk/mcp"
 )
 
+func cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	server := mcp.NewServer(&mcp.Implementation{Name: "dora-web-explorer", Version: "1.0.0"}, nil)
 	mcp.AddTool(server, tools.WebSearchTool, tools.WebSearchHandler)
@@ -20,7 +36,7 @@ func main() {
 	}, nil)
 
 	mux := http.NewServeMux()
-	mux.Handle("/mcp", handler)
+	mux.Handle("/mcp", cors(handler))
 
 	logger.Log.Info("Starting MCP server on :8080...")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
